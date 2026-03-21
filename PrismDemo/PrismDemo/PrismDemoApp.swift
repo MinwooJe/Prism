90985d1e459ee9f1a831258f64d8991333d4f134
MinwooJe <59834326+MinwooJe@users.noreply.github.com>
2026-03-22 03:12:00 +0900

    chore: 데모 앱 추가

---
+++ PrismDemo/PrismDemo/PrismDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrismDemoApp.swift
+//  PrismDemo
+//
+//  Created by MinwooJe on 3/22/26.
+//
+
+import SwiftUI
+
+@main
+struct PrismDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
